55869912dfdc498d3060980700aa83e5c888f134
DineshRajput20 <dineshrajput96485@gmail.com>
2024-10-16 15:31:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div style={{ backgroundColor: bgColor, height: '100vh', transition: 'background-color 0.5s ease' }}>
-      <h1 className="animated-heading">Dinesh Rajput R8 42456</h1>
+      <h1 className="animated-heading">Shashwat P8 42403</h1>
       <button className="change-color-btn" onClick={changeColor}>Change Background Color</button>
     </div>
   );
